07c6907b5ed6595dcc711ba920099c3de08f43eb
leandroandrade <leandro.andrade2588@gmail.com>
2021-05-11 19:46:57 -0300

    use typescript

---
+++ consume-type.ts
@@ -0,0 +1,18 @@
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9094']
+});
+
+(async () => {
+    const consumer = kafka.consumer({groupId: 'test-group'})
+    await consumer.connect()
+
+    await consumer.subscribe({topic: 'bank001', fromBeginning: true})
+    await consumer.run({
+        eachMessage: async ({topic, partition, message}) => {
+            console.log({value: message.value!.toString(),})
+        },
+    })
+})();
---
+++ producer-type.ts
@@ -0,0 +1,20 @@
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['localhost:9094']
+});
+
+(async () => {
+    const producer = kafka.producer()
+    await producer.connect()
+
+    const payload = {
+        topic: 'bank001',
+        messages: [
+            {key: 'kafka-producer', value: `Hello KafkaJS ${ Date.now() }`},
+        ],
+    };
+    await producer.send(payload)
+    await producer.disconnect()
+})();
